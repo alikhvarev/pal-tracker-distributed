67f70d5b3825c8d51e4f9a7a66b52862e62d1a2a
cyndeeshank12 <cyndee.shank@icloud.com>
2019-08-29 11:37:54 -0600

    Enabled Curcuit Breaker in 3 apps that depend on registration app

--- components/allocations/src/main/java/io/pivotal/pal/tracker/allocations/ProjectClient.java
+++ components/allocations/src/main/java/io/pivotal/pal/tracker/allocations/ProjectClient.java
@@ -1,18 +1,34 @@
 package io.pivotal.pal.tracker.allocations;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.web.client.RestOperations;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ProjectClient {
 
     private final RestOperations restOperations;
     private final String registrationServerEndpoint;
 
+    private Map<Long, ProjectInfo> cache = new ConcurrentHashMap<>();
+
     public ProjectClient(RestOperations restOperations, String registrationServerEndpoint) {
-        this.restOperations= restOperations;
+        this.restOperations = restOperations;
         this.registrationServerEndpoint = registrationServerEndpoint;
     }
 
+    @HystrixCommand(fallbackMethod = "getProjectFromCache")
     public ProjectInfo getProject(long projectId) {
-        return restOperations.getForObject(registrationServerEndpoint + "/projects/" + projectId, ProjectInfo.class);
+        ProjectInfo projectInfo = restOperations.getForObject(registrationServerEndpoint + "/projects/" + projectId, ProjectInfo.class);
+        if (projectInfo != null) {
+            cache.put(projectId, projectInfo);
+        }
+        return projectInfo;
     }
+
+    public ProjectInfo getProjectFromCache(long projectId) {
+        return cache.get(projectId);
+    }
+
 }
